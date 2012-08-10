ebb73292e4defa8e1850ea57bea107c9503546f1
Matt Di Pasquale <liveloveprosper@gmail.com>
2012-08-10 00:04:12 -0400

    Redirect all domain aliases to www.myownbed.com.

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
 
+# Permanently (301) redirect myownbed.herokuapp.com et al. to www.myownbed.com.
+# Because, ensuring a unique domain, improves search engine optimization (SEO).
+configure :production do
+  before do
+    subdomain = 'www.myownbed.com'
+    unless request.host_with_port == subdomain
+      redirect "http://#{subdomain}#{request.fullpath}", 301
+    end
+  end
+end
+
 get '/' do
   erb :index
 end
